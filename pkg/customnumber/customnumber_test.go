2888245267fc74987745368ab20f8b5c6c68883a
Christos Petropoulos <chris.petropoulos91@gmail.com>
2020-09-17 19:38:22 +0300

    Improve custom number tests

--- pkg/customnumber/customnumber_test.go
+++ pkg/customnumber/customnumber_test.go
@@ -15,6 +15,17 @@ func TestCustomNumberString(t *testing.T) {
 	}
 }
 
+func TestCustomNumberSmartString(t *testing.T){
+	initialValue:= "000010"
+	want:="10"
+	values:=[]rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	number:= customnumber.NewNumber(values,initialValue)
+
+	if got, want := number.SmartString(), want;got !=want {
+		t.Errorf("Smart string of custom number, want: %s got: %s",want,got)
+	}
+}
+
 func TestIncrement(t *testing.T) {
 	want := "150001"
 	values := []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
