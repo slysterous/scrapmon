825a241685348e2b308df158347b8e0c38e7521e
Christos Petropoulos <chris.petropoulos91@gmail.com>
2021-09-10 12:39:55 +0300

    Apply fmt

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -11,7 +11,7 @@ import (
 const (
 	TraceLevel uint32 = iota
 	DebugLevel uint32 = iota
-	InfoLevel uint32  = iota
+	InfoLevel  uint32 = iota
 	WarnLevel  uint32 = iota
 	ErrorLevel uint32 = iota
 )
@@ -19,24 +19,24 @@ const (
 type color string
 
 const (
-	ColorReset color = "\033[0m"
-	ColorRed color = "\033[31m"
-	ColorGreen color = "\033[32m"
+	ColorReset  color = "\033[0m"
+	ColorRed    color = "\033[31m"
+	ColorGreen  color = "\033[32m"
 	ColorYellow color = "\033[33m"
-	ColorBlue color = "\033[34m"
-	ColorCyan color = "\033[36m"
-	ColorWhite color = "\033[37m"
+	ColorBlue   color = "\033[34m"
+	ColorCyan   color = "\033[36m"
+	ColorWhite  color = "\033[37m"
 )
 
 // Logger is a simple std out logger
 type Logger struct {
-	level uint32
+	level  uint32
 	writer io.Writer
 }
 
-func NewLogger(level uint32,writer io.Writer) Logger {
+func NewLogger(level uint32, writer io.Writer) Logger {
 	return Logger{
-		level: level,
+		level:  level,
 		writer: writer,
 	}
 }
@@ -45,40 +45,40 @@ func (l Logger) GetLevel() uint32 {
 	return l.level
 }
 
-func (l Logger) Tracef(format string, args ...interface{}){
-	if l.GetLevel()<=TraceLevel{
-		printWithColor(l.writer,ColorCyan,format,args)
+func (l Logger) Tracef(format string, args ...interface{}) {
+	if l.GetLevel() <= TraceLevel {
+		printWithColor(l.writer, ColorCyan, format, args)
 	}
 }
 
 // Debugf logs a debug message.
 func (l Logger) Debugf(format string, args ...interface{}) {
 	if l.GetLevel() <= DebugLevel {
-		printWithColor(l.writer,ColorBlue,format,args)
+		printWithColor(l.writer, ColorBlue, format, args)
 	}
 }
 
 // Infof logs an info message.
 func (l Logger) Infof(format string, args ...interface{}) {
 	if l.GetLevel() <= InfoLevel {
-		printWithColor(l.writer,ColorGreen,format,args)
+		printWithColor(l.writer, ColorGreen, format, args)
 	}
 }
 
 // Warnf logs a warning message.
 func (l Logger) Warnf(format string, args ...interface{}) {
 	if l.GetLevel() <= WarnLevel {
-		printWithColor(l.writer,ColorYellow,format,args)
+		printWithColor(l.writer, ColorYellow, format, args)
 	}
 }
 
 // Errorf logs an error message.
 func (l Logger) Errorf(format string, args ...interface{}) {
 	if l.GetLevel() <= ErrorLevel {
-		printWithColor(l.writer,ColorRed,format,args)
+		printWithColor(l.writer, ColorRed, format, args)
 	}
 }
 
-func printWithColor(w io.Writer,color color,format string, args ...interface{}) {
-	fmt.Fprintf(w,string(color)+format+string(ColorReset),args)
+func printWithColor(w io.Writer, color color, format string, args ...interface{}) {
+	fmt.Fprintf(w, string(color)+format+string(ColorReset), args)
 }
--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -8,137 +8,137 @@ import (
 	"testing"
 )
 
-func TestTracef(t *testing.T){
+func TestTracef(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.TraceLevel,&buf)
-		lg.Tracef("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.TraceLevel, &buf)
+		lg.Tracef("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		want := fmt.Sprintf(string(logger.ColorCyan)+"omg this is a test with param: %s"+string(logger.ColorReset),"parameter")
+		want := fmt.Sprintf(string(logger.ColorCyan)+"omg this is a test with param: %s"+string(logger.ColorReset), "parameter")
 
-		if strings.Contains(want,got){
-			t.Errorf("expected: %s, got: %s",want,got)
+		if strings.Contains(want, got) {
+			t.Errorf("expected: %s, got: %s", want, got)
 		}
 	})
-	t.Run("Don't print due to log level",func(t *testing.T){
+	t.Run("Don't print due to log level", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.WarnLevel,&buf)
-		lg.Tracef("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.WarnLevel, &buf)
+		lg.Tracef("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		if got!=""{
-			t.Errorf("expected empty string, got: %s",got)
+		if got != "" {
+			t.Errorf("expected empty string, got: %s", got)
 		}
 	})
 }
 
-func TestDebugf(t *testing.T){
+func TestDebugf(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.DebugLevel,&buf)
-		lg.Debugf("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.DebugLevel, &buf)
+		lg.Debugf("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		want := fmt.Sprintf(string(logger.ColorBlue)+"omg this is a test with param: %s"+string(logger.ColorReset),"parameter")
+		want := fmt.Sprintf(string(logger.ColorBlue)+"omg this is a test with param: %s"+string(logger.ColorReset), "parameter")
 
-		if strings.Contains(want,got){
-			t.Errorf("expected: %s, got: %s",want,got)
+		if strings.Contains(want, got) {
+			t.Errorf("expected: %s, got: %s", want, got)
 		}
 	})
-	t.Run("Don't print due to log level",func(t *testing.T){
+	t.Run("Don't print due to log level", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.WarnLevel,&buf)
-		lg.Debugf("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.WarnLevel, &buf)
+		lg.Debugf("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		if got!=""{
-			t.Errorf("expected empty string, got: %s",got)
+		if got != "" {
+			t.Errorf("expected empty string, got: %s", got)
 		}
 	})
 }
 
-func TestInfof(t *testing.T){
+func TestInfof(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.InfoLevel,&buf)
-		lg.Infof("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.InfoLevel, &buf)
+		lg.Infof("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		want := fmt.Sprintf(string(logger.ColorGreen)+"omg this is a test with param: %s"+string(logger.ColorReset),"parameter")
+		want := fmt.Sprintf(string(logger.ColorGreen)+"omg this is a test with param: %s"+string(logger.ColorReset), "parameter")
 
-		if strings.Contains(want,got){
-			t.Errorf("expected: %s, got: %s",want,got)
+		if strings.Contains(want, got) {
+			t.Errorf("expected: %s, got: %s", want, got)
 		}
 	})
-	t.Run("Don't print due to log level",func(t *testing.T){
+	t.Run("Don't print due to log level", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.WarnLevel,&buf)
-		lg.Debugf("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.WarnLevel, &buf)
+		lg.Debugf("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		if got!=""{
-			t.Errorf("expected empty string, got: %s",got)
+		if got != "" {
+			t.Errorf("expected empty string, got: %s", got)
 		}
 	})
 }
 
-func TestWarnf(t *testing.T){
+func TestWarnf(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.WarnLevel,&buf)
-		lg.Warnf("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.WarnLevel, &buf)
+		lg.Warnf("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		want := fmt.Sprintf(string(logger.ColorGreen)+"omg this is a test with param: %s"+string(logger.ColorReset),"parameter")
+		want := fmt.Sprintf(string(logger.ColorGreen)+"omg this is a test with param: %s"+string(logger.ColorReset), "parameter")
 
-		if strings.Contains(want,got){
-			t.Errorf("expected: %s, got: %s",want,got)
+		if strings.Contains(want, got) {
+			t.Errorf("expected: %s, got: %s", want, got)
 		}
 	})
-	t.Run("Don't print due to log level",func(t *testing.T){
+	t.Run("Don't print due to log level", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.ErrorLevel,&buf)
-		lg.Debugf("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.ErrorLevel, &buf)
+		lg.Debugf("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		if got!=""{
-			t.Errorf("expected empty string, got: %s",got)
+		if got != "" {
+			t.Errorf("expected empty string, got: %s", got)
 		}
 	})
 }
 
-func TestErrorf(t *testing.T){
+func TestErrorf(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(logger.ErrorLevel,&buf)
-		lg.Errorf("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(logger.ErrorLevel, &buf)
+		lg.Errorf("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		want := fmt.Sprintf(string(logger.ColorGreen)+"omg this is a test with param: %s"+string(logger.ColorReset),"parameter")
+		want := fmt.Sprintf(string(logger.ColorGreen)+"omg this is a test with param: %s"+string(logger.ColorReset), "parameter")
 
-		if strings.Contains(want,got){
-			t.Errorf("expected: %s, got: %s",want,got)
+		if strings.Contains(want, got) {
+			t.Errorf("expected: %s, got: %s", want, got)
 		}
 	})
-	t.Run("Don't print due to log level",func(t *testing.T){
+	t.Run("Don't print due to log level", func(t *testing.T) {
 		var buf bytes.Buffer
 
-		lg:=logger.NewLogger(5,&buf)
-		lg.Debugf("omg this is a test with param: %s","parameter")
+		lg := logger.NewLogger(5, &buf)
+		lg.Debugf("omg this is a test with param: %s", "parameter")
 
 		got := buf.String()
-		if got!=""{
-			t.Errorf("expected empty string, got: %s",got)
+		if got != "" {
+			t.Errorf("expected empty string, got: %s", got)
 		}
 	})
 }
